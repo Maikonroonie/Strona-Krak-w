3cc529e636231177d03dfc8374b4ceedbd1343c1
Maikonroonie <119044004+Maikonroonie@users.noreply.github.com>
2022-12-02 11:02:09 +0100

    Add files via upload

---
+++ js.js
@@ -0,0 +1,5 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+const firstAkapit = document.querySelector(".first-akapit");
+firstAkapit.textContent = "first-akapit";
